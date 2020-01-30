951d8f1bc3221f5a9962158eba9243fa3ecdbfe4
newkritsada <newtronetic@gmail.com>
2020-01-31 06:51:03 +0700

    function

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../css/Card.css"
-import { Card } from "react-bootstrap";
+import { Card,Button } from "react-bootstrap";
 
 class AnimalCard extends React.Component {
   render() {
-    const { img, alt, title, text} = this.props;
+    const { img, alt, title, text } = this.props;
     return (
       <Card  className="m-4 card-hover card-image">
         <Card.Img
@@ -16,7 +16,7 @@ class AnimalCard extends React.Component {
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{text}</Card.Text>
-          <Button onClick={()=>this.props._onClick(title)}>Click me!</Button>
+          <Button onClick={() => console.log("Clicked")}>Click me!</Button>
         </Card.Body>
       </Card>
     );
